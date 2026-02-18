57bd92e2511ff0be3fc019dacddb2e2431add9cf
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2026-02-18 17:28:28 +0300

    Initial Commit

---
+++ kimai_desktop_macos/kimai_desktop_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  kimai_desktop_macosApp.swift
+//  kimai_desktop_macos
+//
+//  Created by Игорь Герасимов on 18.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct kimai_desktop_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
